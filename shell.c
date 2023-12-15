cee3191de0e5db301c19e48f3fb6e162139425e0
ThomasAquinas <ozoagu.benedict@gmail.com>
2023-12-15 16:08:42 +0000

    remove string.h from the headers

--- shell.c
+++ shell.c
@@ -4,7 +4,6 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <string.h>
 
 /**
  * main - a UNIX command line interpreter.
@@ -38,10 +37,10 @@ int main(void)
 		{
 			printf("#cisfun$ ");
 			nread = getline(&line, &len, stdin);
-			if (line[strlen(line) - 1] == 10)
-				line[strlen(line) - 1] = '\0';
+			if (line[_strlen(line) - 1] == 10)
+				line[_strlen(line) - 1] = '\0';
 			else
-				line[strlen(line)] = '\0';
+				line[_strlen(line)] = '\0';
 			argv[0] = line, argv[1] = NULL;
 			if (execve(argv[0], argv, environ) == -1)
 				perror("Error:");
